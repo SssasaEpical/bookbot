2f1cf30363735d654164df3250626cb0951b44db
Sssasa <elias6shahin@hotmail.com>
2025-08-19 17:14:22 -0600

    L3 Managed to complete a retrieve file and print function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+# This function retrieves the book
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+# This function utilizes the get_book_text(filepath) funciton to print its contents
+def main(path):
+    print(get_book_text(path))
+
+path = "books/frankenstein.txt"
+
+main(path)
